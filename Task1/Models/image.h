7a5d0c3220c6d8de18ad37fb78094f91b5172b28
Youssef Ashraf <yossifaziz29@gmail.com>
2024-03-03 12:09:44 +0200

    Add Image Model headers

--- Task1/Models/image.h
+++ Task1/Models/image.h
@@ -3,17 +3,19 @@
 
 #include <bits/stdc++.h>
 #include<opencv2/opencv.hpp>
-
+#include<QString>
 using namespace std;
 using namespace cv;
 
 class Image
 {
 private:
-    Mat img;
-    bool empty = true;
-    void loadImage();
-
+    Mat originalImg;
+    Mat processedImg;
+    Mat greyScaledImg;
+    bool isEmpty = true;
+    bool isProcessed;
+    QString path;
 
 public:
 
@@ -24,13 +26,29 @@ public:
     Image(string pth);
 
     // Returns img property
-    Mat getImage();
+    Mat getOriginalImg();
+
+    Mat getProcessedImg();
+
+    Mat getGreyScaledImg();
+
+    void setProcessedImg();
 
     // Returns empty property
     bool isEmpty();
 
+    bool isProcessed();
+
+    //
+    QString getImgPath();
+
+    void setImgPath();
+
     // Load/Update a new image
     void loadImage(string pth);
+
+    void convertToGreyScale();
+
 
     ~Image();
 
