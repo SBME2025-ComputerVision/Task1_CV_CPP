3f5702239fecd31aeac63d38d09c6e302cdb51bd
Ziad Meligy <89343979+ZiadMeligy@users.noreply.github.com>
2024-03-15 00:03:08 +0200

    low freq high freq filter
    low freq high freq filter implementation on main

--- Task1/Models/fourier.cpp
+++ Task1/Models/fourier.cpp
@@ -6,19 +6,19 @@ Fourier::Fourier(){}
 
 Mat Fourier::applyDFT(Mat img){
 
-    int m = getOptimalDFTSize(img.rows);
-    int n = getOptimalDFTSize(img.cols);
+    // int m = getOptimalDFTSize(img.rows);
+    // int n = getOptimalDFTSize(img.cols);
 
-    // Add zero padding to the image to reach the optimal size
-    Mat padded;
-    copyMakeBorder(img, padded, 0, m - img.rows, 0, n - img.cols, BORDER_CONSTANT, Scalar::all(0));
+    // // Add zero padding to the image to reach the optimal size
+    // Mat padded;
+    // copyMakeBorder(img, padded, 0, m - img.rows, 0, n - img.cols, BORDER_CONSTANT, Scalar::all(0));
 
-    // Convert the image to floating point
-    padded.convertTo(padded, CV_32F);
-
+    // // Convert the image to floating point
+    // padded.convertTo(padded, CV_32F);
+    // Mat dftt
     // Perform the DFT
     Mat fourierTransform;
-    dft(padded, fourierTransform, DFT_SCALE|DFT_COMPLEX_OUTPUT);
+    dft(img, fourierTransform, DFT_SCALE|DFT_COMPLEX_OUTPUT);
     return fourierTransform;
 }
 
@@ -31,51 +31,51 @@ Mat Fourier::applyIDFT(Mat img){
 Mat Fourier::applyShifting(Mat img){
 
 
-    Mat fourier_output_img = img.clone();
-    int cx = fourier_output_img.cols / 2;
-    int cy = fourier_output_img.rows / 2;
+    // Mat fourier_output_img = img.clone();
+    // int cx = fourier_output_img.cols / 2;
+    // int cy = fourier_output_img.rows / 2;
 
-    // divide the fourier input img into four parts ----------------------------------
-    // rect ( x, y , w, h)
-    Mat left_up(fourier_output_img, Rect(0, 0, cx, cy));
-    Mat right_up(fourier_output_img, Rect(cx, 0, cx, cy));
-    Mat left_down(fourier_output_img, Rect(0, cy, cx, cy));
-    Mat right_down(fourier_output_img, Rect(cx, cy, cx, cy));
+    // // divide the fourier input img into four parts ----------------------------------
+    // // rect ( x, y , w, h)
+    // Mat left_up(fourier_output_img, Rect(0, 0, cx, cy));
+    // Mat right_up(fourier_output_img, Rect(cx, 0, cx, cy));
+    // Mat left_down(fourier_output_img, Rect(0, cy, cx, cy));
+    // Mat right_down(fourier_output_img, Rect(cx, cy, cx, cy));
 
-    // switch the four parts places to center the fourier transform ------------------
-    Mat switch_variable;
-    left_up.copyTo(switch_variable);
-    right_down.copyTo(left_up);
-    switch_variable.copyTo(right_down);
-    right_up.copyTo(switch_variable);
-    left_down.copyTo(right_up);
-    switch_variable.copyTo(left_down);
+    // // switch the four parts places to center the fourier transform ------------------
+    // Mat switch_variable;
+    // left_up.copyTo(switch_variable);
+    // right_down.copyTo(left_up);
+    // switch_variable.copyTo(right_down);
+    // right_up.copyTo(switch_variable);
+    // left_down.copyTo(right_up);
+    // switch_variable.copyTo(left_down);
 
-    return fourier_output_img;
+    // return fourier_output_img;
 
-    // Mat tmp, q0, q1, q2, q3; // quadrants that will be swapped
+    Mat tmp, q0, q1, q2, q3; // quadrants that will be swapped
 
-    // Mat outputShift = img.clone();
+    Mat outputShift = img.clone();
 
-    // int centerX = img.cols /2;
-    // int centerY = img.rows /2;
-
-
-    // q0 = outputShift(Rect(0, 0, centerX, centerY)); // upper left corner
-    // q1 = outputShift(Rect(centerX, 0, centerX, centerY)); // upper right corner
-    // q2 = outputShift(Rect(0, centerY, centerX, centerY)); // lower left corner
-    // q3 = outputShift(Rect(centerX, centerY, centerX, centerY)); //lower right corner
+    int centerX = img.cols /2;
+    int centerY = img.rows /2;
 
 
-    // // switching q0 by q3 and q1 by q2
-    // q0.copyTo(tmp);
-    // q3.copyTo(q0);
-    // tmp.copyTo(q3);
+    q0 = outputShift(Rect(0, 0, centerX, centerY)); // upper left corner
+    q1 = outputShift(Rect(centerX, 0, centerX, centerY)); // upper right corner
+    q2 = outputShift(Rect(0, centerY, centerX, centerY)); // lower left corner
+    q3 = outputShift(Rect(centerX, centerY, centerX, centerY)); //lower right corner
 
-    // q1.copyTo(tmp);
-    // q2.copyTo(q1);
-    // tmp.copyTo(q2);
 
-    // return outputShift;
+    // switching q0 by q3 and q1 by q2
+    q0.copyTo(tmp);
+    q3.copyTo(q0);
+    tmp.copyTo(q3);
+
+    q1.copyTo(tmp);
+    q2.copyTo(q1);
+    tmp.copyTo(q2);
+
+    return outputShift;
 }
 
--- Task1/main.cpp
+++ Task1/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 
     Mat dftImg = Fourier::applyDFT(grey);
 
-   dftImg = Fourier::applyShifting(dftImg);
+   // dftImg = Fourier::applyShifting(dftImg);
 
     Mat planes[2];
     split(dftImg,planes);
@@ -34,11 +34,45 @@ int main(int argc, char *argv[])
     phase(planes[0],planes[1],phaz,true);
 
 
+    log(mag,mag);
+    mag *=20;
+    normalize(mag,mag,0,1,NORM_MINMAX);
+    Mat real, imag;
+    real = Fourier::applyShifting(planes[0]);
+    imag = Fourier::applyShifting(planes[1]);
 
 
-    Mat img = Fourier::applyIDFT(phaz);
+    int cx = mag.cols / 2;
+    int cy = mag.rows / 2;
+
+    // Define the radius of the central region to keep
+    int radius = 100; // You can adjust this value according to your requirement
 
 
+
+    // Create a mask to zero out frequencies outside the central region
+    Mat mask = Mat::zeros(mag.size(), CV_8U);
+
+    circle(mask, Point(cx, cy), radius, Scalar(255), -1);
+
+    Mat highPassMask;
+    bitwise_not(mask, highPassMask);
+
+    // Apply the mask to the magnitude spectrum
+    real.setTo(Scalar(0), ~highPassMask);
+    imag.setTo(Scalar(0), ~highPassMask);
+
+    Mat shifted_DFT_plane_img[] = {real ,imag };
+
+    Mat MergeImg;
+    merge(shifted_DFT_plane_img, 2, MergeImg);
+
+
+
+
+    Mat img = Fourier::applyIDFT(MergeImg);
+
+    // cout << fshift;
 
     imwrite("inversing.jpg",img);
     return 0;
