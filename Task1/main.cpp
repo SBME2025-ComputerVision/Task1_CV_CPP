85fc16dcfb60b25f17a6a30f8a8c0e8b235ee68e
minicoders <yossifaziz29@gmail.com>
2024-03-18 02:16:28 +0200

    Add Styling
    - Add Style sheet
    Note dont forget to add the qss file in gallery
    to the Build directory
    - Add Borders & Some Styling

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -7,11 +7,23 @@
 #include<Models/fourier.h>
 #include"config.h"
 #include <QApplication>
+#include <QFile>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    //  import style sheets
+        QFile file("ManjaroMix.qss");
+        file.open(QFile::ReadOnly);
+
+        QString styleSheet { QLatin1String(file.readAll()) };
+
+        //setup stylesheet
+        a.setStyleSheet(styleSheet);
+    //
+
     return a.exec();
 
 
