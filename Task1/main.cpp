61e10f9b8d4974f20693319eb3d82468c54ca6b0
yoyobunt <yossifaziz29@gmail.com>
2024-03-31 00:36:33 +0200

    Update main.cpp

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -8,30 +8,30 @@
 
 int main(int argc, char *argv[])
 {
-    // QApplication  a(argc, argv);
-    // MainWindow w;
-    //  w.setWindowState(Qt::WindowMaximized);
-    // w.show();
+     QApplication  a(argc, argv);
+     MainWindow w;
+      w.setWindowState(Qt::WindowMaximized);
+     w.show();
 
 
-    // //  import style sheets
-    //     QFile file("ManjaroMix.qss");
-    //     file.open(QFile::ReadOnly);
+     //  import style sheets
+         QFile file("ManjaroMix.qss");
+         file.open(QFile::ReadOnly);
 
-    //     QString styleSheet { QLatin1String(file.readAll()) };
+         QString styleSheet { QLatin1String(file.readAll()) };
 
-    //     //setup stylesheet
-    //     a.setStyleSheet(styleSheet);
-    // //
+         //setup stylesheet
+         a.setStyleSheet(styleSheet);
+     //
 
 
-    // return a.exec();
+     return a.exec();
 
-    Image* img = new Image("./Gallery/f.jpg");
+//    Image* img = new Image("./Gallery/f.jpg");
 
-    Mat ori = img->getOriginalImg();
-    Mat v = HoughTransform::detectLines(ori,150);
+//    Mat ori = img->getOriginalImg();
+//    Mat v = HoughTransform::detectLines(ori,150);
 
-    imwrite("beta3y.jpg",v);
+//    imwrite("emad.jpg",v);
     return 5;
 }
