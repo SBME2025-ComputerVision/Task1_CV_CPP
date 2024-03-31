e67daa13643ddab78a4c4b59ff38df055f2bbe5a
Mourad Magdy <murad.ibrahim02@eng-st.cu.edu.eg>
2024-03-31 21:05:12 +0200

    canny implementation

--- Task1/Models/edgedetection.h
+++ Task1/Models/edgedetection.h
@@ -5,10 +5,13 @@
 #include<opencv2/opencv.hpp>
 using namespace cv;
 
+#define STRONG_EDGE 255
+#define WEAK_EDGE 40
 class EdgeDetection
 {
 public:
     EdgeDetection();
+
 
     static Mat detectEdgeSobel(Mat img);
     static Mat detectEdgeSobelX(Mat img);
@@ -22,10 +25,18 @@ public:
     static Mat detectEdgePrewittX(Mat img);
     static Mat detectEdgePrewittY(Mat img);
 
+    static Mat edgeMagnitude(Mat edgeX, Mat edgeY);
+    static Mat Phase_Gradient(Mat gradient_x,Mat gradient_y);
+
+
+
+    static Mat hysteresis(Mat thresholded);
+    static Mat doubleThresholding(Mat suppressed,float lowThreshold,float HighThreshold);
+    static Mat supressionNonMax(Mat magnitudeGradient,Mat phaseGradient);
     static Mat detectEdgeCanny(Mat img ,int t_low ,int t_high);
 
 
-    static Mat edgeMagnitude(Mat edgeX, Mat edgeY);
+
 };
 
 #endif // EDGEDETECTION_H
