23064610eeb725e7a1ea2db1ead1b564ed9818e1
Abdulrahman Emad <104274128+abduelrahmanemad@users.noreply.github.com>
2024-04-26 23:05:31 +0300

    added region growing files

---
+++ Task1/UI/regiongrowingwidget.cpp
@@ -0,0 +1,3 @@
+#include "regiongrowingwidget.h"
+
+RegionGrowingWidget::RegionGrowingWidget() {}
---
+++ Task1/UI/regiongrowingwidget.h
@@ -0,0 +1,10 @@
+#ifndef REGIONGROWINGWIDGET_H
+#define REGIONGROWINGWIDGET_H
+
+class RegionGrowingWidget
+{
+public:
+    RegionGrowingWidget();
+};
+
+#endif // REGIONGROWINGWIDGET_H
